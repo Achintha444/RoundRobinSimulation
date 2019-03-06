2d876a3fe072509da8b2b7e03efece40e7f95426
Achintha <achinthaisuru444@gmail.com>
2019-03-06 16:13:24 +0530

    Create the GUI

---
+++ src/Interface.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Interface {
+    private JPanel panelInterface;
+    private JEditorPane titleEditorPane;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
