56ce3d3c0415bddbd7f3ec30c7e6ed0da9876184
Achintha444 <46097917+Achintha444@users.noreply.github.com>
2019-04-04 10:33:51 +0530

    Update RoundRobin.java

--- RoundRobin.java
+++ RoundRobin.java
@@ -1,5 +1,6 @@
 package roundrobinsimulation;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,20 +10,20 @@ import java.util.Queue;
 public class RoundRobin {
 
     private ArrayList<Process> processList;
+    private ArrayList<String[]> timingDetails;
     private int timeQuantum;
     private Queue<Process> init;
 
-    public RoundRobin(ArrayList<Process> processList, int timeQuantum) {
+    public RoundRobin(ArrayList<Process> processList) {
         this.processList = processList;
-        this.timeQuantum = timeQuantum;
         this.init = new LinkedList<>();
+        this.timingDetails = new ArrayList<>();
     }
-    
-    
-    
+       
     private ArrayList<Process> calculate(){
         
         ArrayList<Process> completedProcessList = new ArrayList<>();
+        this.timeQuantum = this.calculateTimeQuantum();
         
         //Adding the process to the queue
         int temp ;
@@ -36,8 +37,12 @@ public class RoundRobin {
         
         //Round Robin Process
         int totalTime = 0; //total competion time
+        System.out.println(this.timeQuantum);
+        
         while(true){
-            
+            String tempInit;
+            String tempEndT;
+            String tempProcessName;
             
             if (init.isEmpty()){
                 break;
@@ -50,23 +55,63 @@ public class RoundRobin {
                     totalTime+=1;
                 }
                 if(tempProcess.getBurstTimeTemp()<=this.timeQuantum){
+                    tempProcessName = tempProcess.getName();
+                    tempInit=Integer.toString(totalTime);
                     totalTime+=tempProcess.getBurstTimeTemp();
+                    tempEndT=Integer.toString(totalTime);
                     tempProcess.setCompletedTime(totalTime);
                     int tempTAT = totalTime-tempProcess.getArrivalTime();
                     tempProcess.setTurnAroundTime(tempTAT);
                     tempProcess.setWaitingTime(tempTAT-tempProcess.getBurstTime());
                     completedProcessList.add(tempProcess);
                     
+                    String[] tempArray = {tempInit, tempEndT, tempProcessName};
+                    this.timingDetails.add(tempArray);
+                    
                 }
                 else{
+                    tempProcessName = tempProcess.getName();
+                    tempInit=Integer.toString(totalTime);
+                    
                     tempProcess.setBurstTimeTemp(tempProcess.getBurstTimeTemp()-this.timeQuantum);
                     totalTime+=this.timeQuantum;
+                     tempEndT=Integer.toString(totalTime);
                     init.add(tempProcess);
+                    
+                    String[] tempArray = {tempInit, tempEndT, tempProcessName};
+                    this.timingDetails.add(tempArray);
                 }
             }
-            System.out.println(completedProcessList.size());
+            //System.out.println(completedProcessList.size());
         }
         return completedProcessList;    
+    }
+    
+    private int calculateTimeQuantum(){
+        System.out.println(this.processList.size());
+        int max = this.processList.get(0).getBurstTime();
+        int min = this.processList.get(0).getBurstTime();
+        int totalBT = this.processList.get(0).getBurstTime();
+        for(int i=1; i<5; i++){
+            totalBT += this.processList.get(i).getBurstTime();
+            if(max<this.processList.get(i).getBurstTime()){
+                max = this.processList.get(i).getBurstTime();
+            }
+            else if(min>this.processList.get(i).getBurstTime()){
+                min = this.processList.get(i).getBurstTime();
+            }
+        }
+        double c = (max-min)/2;
+        double z = (totalBT)/5;
+        int C = (int)(Math.ceil(c));
+        int Z = (int)(Math.ceil(z));        
+        
+        if (C<=Z && C!=0){
+            return C;
+        }
+        else{
+            return Z;
+        }
     }
     
     private int calculateMin(){
@@ -88,6 +133,16 @@ public class RoundRobin {
         return completedProcessList;
         
     }
+
+    public int getTimeQuantum() {
+        return timeQuantum;
+    }
+    
+    public ArrayList<String[]> getTiminigDetails(){
+        return this.timingDetails;
+    }
+    
+    
 
 }
 
